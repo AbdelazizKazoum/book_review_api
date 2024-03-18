5efe61c3d92fa84b42b45f9e8d76c4f8093c1d05
Abdelaziz kazoum <abdukazoum@gmail.com>
2024-03-18 04:13:17 +0100

    add delete update review

--- routes/auth_users.js
+++ routes/auth_users.js
@@ -1,4 +1,6 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import books from "./booksdb.js";
 
 const regd_users = express.Router();
 
@@ -12,8 +14,10 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-  //returns boolean
-  //write code to check if username and password match the one we have in records.
+  const userExists = users.find((item) => item.username === username);
+  if (userExists) return true;
+
+  return false;
 };
 
 //only registered users can login
@@ -21,8 +25,17 @@ regd_users.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
+  const findUser = users.find((item) => item.username === username);
+
   if (username && password) {
-    if (userExists) {
+    if (authenticatedUser(username, password)) {
+      const accesstoken = jwt.sign({ user: req.body }, "secret", {
+        expiresIn: 60 * 60,
+      });
+
+      req.session.authorization = { accesstoken };
+
+      return res.status(200).json({ message: "User successfully loged in " });
     } else {
       res.status(401).json({ message: "Invalid credentials!" });
     }
@@ -30,6 +43,38 @@ regd_users.post("/login", (req, res) => {
     res.status(400).json({ message: "Enter a valid username and password !" });
     return;
   }
+});
+
+// add a book review
+regd_users.post("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  let token = req.session.authorization["accesstoken"];
+
+  jwt.verify(token, "secret", (err, data) => {
+    if (!err) {
+      books[isbn].reviews[data.user.username] = review;
+      return res.status(200).json({ data: books[isbn].reviews });
+    } else {
+      return res.status(401).json({ message: "Log in first" });
+    }
+  });
+});
+
+// Delete review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const token = req.session.authorization["accesstoken"];
+
+  jwt.verify(token, "secret", (err, data) => {
+    if (!err) {
+      console.log(books[isbn]?.review[data.user.username]);
+      // delete books[isbn]?.review[data.user.username];
+      return res.status(200).json({ data: books[isbn].review });
+    } else {
+      return res.status(401).json({ message: "log in first" });
+    }
+  });
 });
 
 export default regd_users;
